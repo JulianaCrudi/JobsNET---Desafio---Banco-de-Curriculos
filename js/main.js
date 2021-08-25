48e0eb74ebd02d35d08db4eb64f069245417ed7f
Juliana Crudi <julianacrudi@hotmail.com>
2021-08-25 20:49:42 -0300

    corrigindo o bug do cpf e rg

--- js/main.js
+++ js/main.js
@@ -31,24 +31,20 @@ document.getElementById('cep').addEventListener('focusout',pesquisarCep)
 
 /*VALIDAÇÃO CPF*/
 
-const cpf = document.getElementById('cpf').value;
-document.getElementById('cpf').addEventListener('focusout',validaCPF)
-
-function validaCPF(cpf) {
-  if (cpf.length != 11) {
-     alert('CPF INVALIDO! POR FAVOR DIGITE UM CPF VÁLIDO COM 11 DIGITOS (APENAS NÚMEROS)')
-  }
-  else{
-    return true;
+const validaCPF = async() =>{
+  const cpf = document.getElementById('cpf').value
+    if (cpf.length != 11) {
+      alert('CPF INVALIDO! POR FAVOR DIGITE UM CPF VÁLIDO COM 11 DIGITOS (APENAS NÚMEROS)')
+  } else{
+    return true
   }
 }
+document.getElementById('cpf').addEventListener('focusout',validaCPF)
 
 /*VALIDAÇÃO DO RG*/
 
-const rg = document.getElementById('rg').value;
-document.getElementById('rg').addEventListener('focusout',validaRG)
-
-function validaRG(rg) {
+const validaRG = async() => {
+  const rg = document.getElementById('rg').value;
   if (rg.length != 9) {
      alert('RG INVALIDO! POR FAVOR DIGITE UM RG VÁLIDO COM 9 DIGITOS (APENAS NÚMEROS)')
   }
@@ -56,3 +52,4 @@ function validaRG(rg) {
     return true;
   }
 }
+document.getElementById('rg').addEventListener('focusout',validaRG)
